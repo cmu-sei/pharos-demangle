e459b85ce81c29b05c615227948de22703eb31f2
Michael Welsh Duggan <mwd@cert.org>
2017-11-16 12:21:07 -0500

    Whitespace cleanup

--- src/pydemanglemodule.cpp
+++ src/pydemanglemodule.cpp
@@ -32,31 +32,31 @@ PyObject* pythondemangle(char* mangled)
 {
     std::stringstream final_out_stream("");
     try {
-        
+
         std::unique_ptr<demangle::JsonOutput> json_output;
         auto builder = json::simple_builder();
         json_output = std::unique_ptr<demangle::JsonOutput>(new JsonOutput(*builder));
-        
+
         bool debug = false;
         auto t = demangle::visual_studio_demangle(mangled, debug);
-        
+
         auto node = json_output->minimal(*t);
         node->add("symbol", mangled);
         //node->add("demangled", str(*t));
-        
+
         final_out_stream << *node;
-        
+
     }
     catch (const demangle::Error& e) {
-        
+
         final_out_stream << "{}";
-        
+
     }
-    
+
     // necessary to copy the string or we lose it (temp memory)
     const std::string tmp = final_out_stream.str();
     const char* final_out = tmp.c_str();
-    
+
     // convert to cstring for python
     return PyUnicode_FromString(final_out);
 }
@@ -65,4 +65,4 @@ BOOST_PYTHON_MODULE(pydemangle)
 {
     def("demangle", pythondemangle);
 }
- 
+
