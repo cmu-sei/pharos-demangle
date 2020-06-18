420e032f76084609ba7957a175f93e7b0c50257e
Edward J. Schwartz <eschwartz@cert.org>
2020-06-18 10:14:12 -0400

    Selectively use warning flags on unix

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 #from distutils.core import setup
 from distutils.extension import Extension
+from distutils.command.build_ext import build_ext
 
 import os
 import sys
@@ -18,6 +19,20 @@ __version__ = '1.2'
 libraries = ['boost_python'] if sys.version_info[0] < 3 else ['boost_python3']
 libraries += ['boost_system','boost_filesystem','boost_program_options']
 
+class BuildExt(build_ext):
+
+    def build_extensions(self):
+        ct = self.compiler.compiler_type
+        opts = []
+        if ct == 'unix':
+            # only add flags which pass the flag_filter
+            opts += ["-Wextra", "-Wshadow", "-Wstrict-aliasing"]
+
+        for ext in self.extensions:
+            ext.extra_compile_args = opts
+
+        build_ext.build_extensions(self)
+
 pydemanglemodule = Extension("pydemangle",
                         define_macros = [('MAJOR_VERSION', '1'),
                                      ('MINOR_VERSION', '2')],
@@ -25,10 +40,11 @@ pydemanglemodule = Extension("pydemangle",
                         libraries = libraries,
                         library_dirs = [os.getcwd(),],
                         sources = ['libdemangle/codes.cpp', 'libdemangle/json.cpp', 'libdemangle/demangle_json.cpp', 'libdemangle/demangle.cpp', 'libdemangle/demangle_text.cpp', 'src/pydemanglemodule.cpp'],
-                        extra_compile_args=["-std=c++11", "-Wall", "-Wextra", "-Wshadow", "-Wstrict-aliasing"],
+                        extra_compile_args=["-std=c++11", "-Wall"],
                         language='c++11')
 
 setup(name='pydemangle',
+      cmdclass=dict(build_ext=BuildExt),
       version=__version__,
       include_package_data=True,
       description='Python interface for Pharos Demangler',
